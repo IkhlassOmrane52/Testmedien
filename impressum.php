131ff2075cada3f1918092918ce9663e6c4fb35f
Ikhlass <omrane.ikhlass@gmail.com>
2023-07-20 12:07:44 +0200

    first project

---
+++ impressum.php
@@ -0,0 +1,78 @@
+<html lang="de">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" type="image/png" sizes="32x32" href="./img/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="./img/favicon-16x16.png">
+		<title>Impressum P. Mohr e. K. Bauschreinerei-Holzbau</title>
+		<link rel="stylesheet" href="./css/normalize.css">
+		<link rel="stylesheet" href="./css/style.css">
+	</head>
+
+	<body class="impressum">
+		<header>
+			<div class="omw-card">
+				<figure class="omw-image">
+					<img src="./img/holzbau-mohr_logo_optimized.png" alt="Logo von P. Mohr e. K. Bauschreinerei-Holzbau">
+				</figure>
+				<div class="omw-content">
+					<h1>P. Mohr e. K. Bauschreinerei-Holzbau</h1>
+					<name>Inh. Michael Mohr </name>
+					<address>Otto-Hahn-Str. 6, 78234 Engen-Welschingen</address>
+					<a href="tel:+497733977708">Tel. 07733-977708</a>
+					<a href="mailto:mail@holzbau-mohr.info">mail@holzbau-mohr.info</a>
+				</div>
+			</div>
+		</header>
+
+		<main>
+			<h1>Impressum</h1>
+
+			<h2>Angaben gemäß § 5 TMG</h2>
+			<p>Michael Mohr<br />
+			P. Mohr  e. K.  Bauschreinerei-Holzbau<br />
+			Otto-Hahn-Str. 6<br />
+			78234 Engen-Welschingen</p>
+
+			<p>Handelsregister: HRA 705394 / 0054548<br />
+			Registergericht: Freiburg</p>
+
+			<h2>Kontakt</h2>
+			<p>Telefon: 07733/977708<br />
+			Telefax: 07733/977709<br />
+			E-Mail: mail@holzbau-mohr.info</p>
+
+			<h2>Umsatzsteuer-ID</h2>
+			<p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br />
+			DE316072732</p>
+
+			<h2>Aufsichtsbehörde</h2>
+			<p>Handwerkskammer Konstanz<br />
+			Webersteig 3<br />
+			78462 Konstanz</p>
+			<p><a href="https://www.hwk-konstanz.de/" target="_blank" rel="noopener noreferrer">https://www.hwk-konstanz.de/</a></p>
+
+			<h2>Angaben zur Berufs­haftpflicht­versicherung</h2>
+			<p><strong>Name und Sitz des Versicherers:</strong><br />
+			ERGO Versicherung AG<br />
+			40198 Düsseldorf</p>
+			<p><strong>Geltungsraum der Versicherung:</strong><br />Deutschland</p>
+
+			<h2>EU-Streitschlichtung</h2>
+			<p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+			<h2>Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h2>
+			<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+		</main>
+
+		<footer>
+			<p>© 2022
+				<a href="index.html">P. Mohr e. K. Bauschreinerei-Holzbau</a> und 
+				<a href="https://medienmacher-bwg.de/" target="_blank">Medienmacher </a>|
+				<a href="impressum.html">Impressum </a>|
+				<a href="datenschutz.html">Datenschutz</a>
+			</p>
+		</footer>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Engber-Spritzgiesstechnik</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header>
+        <figure><img src="./img/Logo_Engber_Entwurf.svg" alt="Logo Engber Spritzgießtechnik"></figure>
+        <div class="wrap-kontakt">
+            <a class="mail" href="mailto:hans.engber-spritzgiesstechnik@t-online.de">
+                <figure><img src="./img/mail.svg" alt=""></figure>
+            </a>
+            <a class="tel" href="tel:+7736924483">
+                <figure><img src="./img/telefon.svg" alt=""></figure>
+            </a>
+        </div>
+    </header>
+    <main>
+        <h1>Unsere Webseite ist bald<br><span>formvollendet!</span></h1>
+    </main>
+    <footer>
+        <div class="wrap-copy">
+            <p>Copyright © 2023 Engber Spritzgießtechnik &</p>
+            <figure><img src="./img/MedienMacher_Logo_DACHMARKE_negativ.png" alt=""></figure>
+        </div>
+        <div class="wrap-imp">
+            <p><a href="impressum.php">Impressum</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="datenschutz.php">Datenschutz</a></p>
+        </div>
+    </footer>
+</body>
+</html>
